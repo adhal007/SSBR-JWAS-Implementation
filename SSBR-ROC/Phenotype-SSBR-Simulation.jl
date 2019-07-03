fa4c741f5ebe1870293fb701bc5b879c1da4b52c
abhilash dhal <adhal@ucdavis.edu>
2019-07-03 12:01:49 -0700

    update pedigree pre-process

--- SSBR-ROC/Phenotype-SSBR-Simulation.jl
+++ SSBR-ROC/Phenotype-SSBR-Simulation.jl
@@ -40,34 +40,13 @@ function ped_ssbr_process(pedfile)
   ped_size = size(pedfile, 1)
   ped_Array = Array{String}(undef, ped_size + 1);
   ped_Array[1] = "ID,Sire,Dam";
-  temp_arr = Array{String}(undef, 3)
-  f = (x) -> Int(x);
-  @time for i in 1:ped_size
-      y = pedfile[i, :]
-      y = f.(y)
-      y = string(y)
-      z = ""
-      s = split(y, "")
-      l = length(s)
-      for j in 1:length(s)
-          if j !== 1 && j !== length(s) && y[j] !== ' '
-           z = z*y[j]
-          end
-      end
-
-# this converts "[1, 2, 3]" to "1,2,3"
-      ped_Array[i + 1] = z
+  f = Int64.(pedfile)
+  g = string.(f)
+  g = "a".*(g)
+  for j in 2:ped_size+1
+    ped_Array[j] = g[j-1, 1]*","*g[j-1, 2]*","*g[j-1, 3]
   end
- ped_size = size(ped_Array, 1)
-new_Arr = Array{String}(undef, ped_size);
-for j in 2:ped_size
-	x = split(string(ped_Array[j]), ",")
-	x = "a" .* x
-	#println(x)
-	new_Arr[j] = x[1]*","*x[2]*","*x[3]
-end
-new_Arr[1] = "ID,sire,dam"
-   return new_Arr
+  return ped_Array
 end
 
 ##################################################################################
